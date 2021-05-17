b8581b4f72309037d0182266571e527a0dec86c5
Robert Niznik <robert.niznik@ithaka.org>
2021-05-17 13:11:20 -0400

    refactor: remove component guidelines from Storybook (#20)
    on-behalf-of: @ithaka <robert.niznik@ithaka.org>

--- packages/pharos/src/pages/reports/react/reports.pages.stories.tsx
+++ packages/pharos/src/pages/reports/react/reports.pages.stories.tsx
@@ -38,12 +38,7 @@ export const Reports: FC = () => (
         <div className="reports-page__container--top">
           <div className="reports-page__container--nav-header">
             <PharosSidenavButton />
-            <img
-              src="./images/storybook/reports/jstor-horizontal.svg"
-              alt="logo"
-              width="96"
-              height="24"
-            />
+            <img src="./images/reports/jstor-horizontal.svg" alt="logo" width="96" height="24" />
             <span className="reports-page__separator">/</span>
             <PharosHeading level={1} preset="4" noMargin>
               Admin
