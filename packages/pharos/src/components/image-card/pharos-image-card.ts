b56db89006053d8b2a0d43311e02a08e31cc5c44
Robert Niznik <robert.niznik@ithaka.org>
2021-05-20 11:32:59 -0400

    feat(pharos): add error and subtle props
    on-behalf-of: @ithaka <robert.niznik@ithaka.org>

--- packages/pharos/src/components/image-card/pharos-image-card.ts
+++ packages/pharos/src/components/image-card/pharos-image-card.ts
@@ -1,11 +1,13 @@
 import { html, LitElement, property } from 'lit-element';
 import type { TemplateResult, CSSResultArray } from 'lit-element';
+import { nothing } from 'lit-html';
 import { imageCardStyles } from './pharos-image-card.css';
 import { designTokens } from '../../styles/variables.css';
 import { customElement } from '../../utils/decorators';
 
 import '../heading/pharos-heading';
 import '../link/pharos-link';
+import '../icon/pharos-icon';
 
 @customElement('pharos-image-card')
 export class PharosImageCard extends LitElement {
@@ -23,18 +25,50 @@ export class PharosImageCard extends LitElement {
   @property({ type: String, reflect: true })
   public link = '';
 
+  /**
+   * Indicates if the image is not available or accessible.
+   * @attr error
+   */
+  @property({ type: Boolean, reflect: true })
+  public error = false;
+
+  /**
+   * Indicates if the metadata should only show on hover of the image.
+   * @attr subtle
+   */
+  @property({ type: Boolean, reflect: true })
+  public subtle = false;
+
   public static get styles(): CSSResultArray {
     return [designTokens, imageCardStyles];
   }
 
+  private _renderImage(): TemplateResult {
+    return this.error
+      ? html`<div class="card__container--error">
+          <pharos-icon name="exclamation-inverse"></pharos-icon>Preview not available
+        </div>`
+      : html`<pharos-link class="card__image" href="${this.link}" subtle flex
+          ><slot name="image"></slot>${this.subtle
+            ? html`<div class="card__metadata--hover"><slot name="metadata"></slot></div>`
+            : nothing}</pharos-link
+        >`;
+  }
+
+  protected get renderTitle(): TemplateResult {
+    return html`<pharos-link class="card__title" href="${this.link}" subtle flex
+      ><pharos-heading preset="1--bold" level="3" no-margin
+        >${this.title}</pharos-heading
+      ></pharos-link
+    >`;
+  }
+
   protected render(): TemplateResult {
     return html`<div class="card">
-      <pharos-link class="card__image" href="${this.link}" subtle flex><slot name="image"></slot></pharos-link>
-      <pharos-link class="card__title" href="${this.link}" subtle flex
-        ><pharos-heading preset="1--bold" level="3" no-margin
-          >${this.title}</pharos-heading
-        ></pharos-link
-      ><div class="card__metadata"><slot name="metadata"></div></slot>
+      ${this._renderImage()} ${this.renderTitle}
+      ${this.subtle
+        ? nothing
+        : html`<div class="card__metadata"><slot name="metadata"></slot></div>`}
     </div>`;
   }
 }
