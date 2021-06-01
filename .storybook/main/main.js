a09da642715730a284246a427c6c2e40aba6fcf3
Niznikr <robert.niznik@ithaka.org>
2021-06-01 16:31:45 -0400

    ci: add main Storybook
    on-behalf-of: @ithaka <robert.niznik@ithaka.org>

---
+++ .storybook/main/main.js
@@ -0,0 +1,14 @@
+let config = require('../main');
+
+config.refs = {
+  story1: {
+    title: 'Web Components',
+    url: 'http://pharos-storybooks.apps.test.cirrostratus.org/wc',
+  },
+  story2: {
+    title: 'React',
+    url: 'http://pharos-storybooks.apps.test.cirrostratus.org/react',
+  },
+};
+
+module.exports = config;
