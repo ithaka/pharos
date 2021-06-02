e2cccd7c8d523c9f3c6c19a0b9271aa7913eaf81
Robert Niznik <robert.niznik@ithaka.org>
2021-06-02 16:59:43 -0400

    ci: add a composed Storybook build (#60)
    on-behalf-of: @ithaka <robert.niznik@ithaka.org>

---
+++ .storybook/main/main.js
@@ -0,0 +1,30 @@
+let config = require('../main');
+
+config.stories.push('../../packages/pharos/**/*.docs.stories.mdx');
+
+config.refs = (config, { configType }) => {
+  if (configType === 'DEVELOPMENT') {
+    return {
+      webComponents: {
+        title: 'Web Components',
+        url: 'http://localhost:9000',
+      },
+      react: {
+        title: 'React',
+        url: 'http://localhost:9001',
+      },
+    };
+  }
+  return {
+    webComponents: {
+      title: 'Web Components',
+      url: 'https://pharos.jstor.org/storybooks/wc/',
+    },
+    react: {
+      title: 'React',
+      url: 'https://pharos.jstor.org/storybooks/react/',
+    },
+  };
+};
+
+module.exports = config;
