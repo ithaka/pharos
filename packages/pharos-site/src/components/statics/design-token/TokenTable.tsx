aedc1f6b9dea0f5c1dd7ecefb3e9ed3afec2f966
Sayem Quazi <sayem.quazi@ithaka.org>
2021-06-02 19:25:06 -0400

    docs: update TokenTable component with useEffect

--- packages/pharos-site/src/components/statics/design-token/TokenTable.tsx
+++ packages/pharos-site/src/components/statics/design-token/TokenTable.tsx
@@ -1,20 +1,30 @@
 import { tokenTable } from './TokenTable.module.css';
-import { PharosHeading } from '@ithaka/pharos/lib/react-components/heading/pharos-heading';
+import { useEffect, useState } from 'react';
 
 export const TokenTable = (
   title: string,
   content: JSX.Element,
   subtitle?: JSX.Element
 ): JSX.Element => {
-  return (
-    <div>
-      <PharosHeading level={1} preset={'6'}>
-        {title}
-      </PharosHeading>
-      {subtitle ? <div>{subtitle}</div> : null}
-      <table className={tokenTable}>{content}</table>
-      <br />
-      <br />
-    </div>
-  );
+  const [StateTable, setStateTable] = useState<JSX.Element>(<></>);
+  const Pharos =
+    typeof window !== `undefined` ? require('@ithaka/pharos/lib/react-components') : null;
+
+  useEffect(() => {
+    const { PharosHeading } = Pharos;
+    const table = (
+      <div>
+        <PharosHeading level={1} preset={'6'}>
+          {title}
+        </PharosHeading>
+        {subtitle ? <div>{subtitle}</div> : null}
+        <table className={tokenTable}>{content}</table>
+        <br />
+        <br />
+      </div>
+    );
+    setStateTable(table);
+  }, [Pharos, title, content, subtitle]);
+
+  return StateTable;
 };
