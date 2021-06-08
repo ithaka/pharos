4b4ea34f5b40e789a648fb4a23c6ce43983fb59c
Niznikr <robert.niznik@ithaka.org>
2021-06-08 08:54:44 -0400

    ci: add ignore config
    on-behalf-of: @ithaka <robert.niznik@ithaka.org>

---
+++ build-ignore.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+[ "$SITE_NAME" == "pharos-storybooks" ] && git diff --quiet HEAD^ HEAD packages/pharos/ .storybook/ package.json yarn.lock || [ "$SITE_NAME" == "pharos" ] && git diff --quiet HEAD^ HEAD packages/pharos-site/ package.json yarn.lock
