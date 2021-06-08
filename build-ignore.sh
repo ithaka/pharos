622cf52d650414466b74a74541a0cf2673517fa9
Niznikr <robert.niznik@ithaka.org>
2021-06-08 10:00:22 -0400

    refactor: simplify syntax
    on-behalf-of: @ithaka <robert.niznik@ithaka.org>

--- build-ignore.sh
+++ build-ignore.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-[ "$SITE_NAME" == "pharos-storybooks" ] && git diff --quiet HEAD^ HEAD packages/pharos/ .storybook/ package.json yarn.lock netlify.toml || [ "$SITE_NAME" == "pharos" ] && git diff --quiet HEAD^ HEAD packages/pharos-site/ package.json yarn.lock netlify.toml
+if [ "$SITE_NAME" == "pharos-storybooks" ]
+then
+  git diff --quiet HEAD^ HEAD packages/pharos/ .storybook/ package.json yarn.lock netlify.toml
+elif [ "$SITE_NAME" == "pharos" ]
+then
+  git diff --quiet HEAD^ HEAD packages/pharos-site/ package.json yarn.lock netlify.toml
+fi
