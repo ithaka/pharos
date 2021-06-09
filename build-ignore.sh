259a6956cdc487434387144751548bc9abc71645
Niznikr <robert.niznik@ithaka.org>
2021-06-09 09:03:46 -0400

    ci: use last built commit for ignore
    on-behalf-of: @ithaka <robert.niznik@ithaka.org>

--- build-ignore.sh
+++ build-ignore.sh
@@ -2,8 +2,8 @@
 
 if [ "$SITE_NAME" == "pharos-storybooks" ]
 then
-  git diff --quiet HEAD^ HEAD packages/pharos/ .storybook/ package.json yarn.lock netlify.toml
+  git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF packages/pharos/ .storybook/ package.json yarn.lock netlify.toml
 elif [ "$SITE_NAME" == "pharos" ]
 then
-  git diff --quiet HEAD^ HEAD packages/pharos-site/ package.json yarn.lock netlify.toml
+  git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF packages/pharos-site/ package.json yarn.lock netlify.toml
 fi
