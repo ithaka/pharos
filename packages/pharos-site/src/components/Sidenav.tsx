ababea2a2a7bd17d6de2ecde2ffde6f015f9ba19
Elham Islam <elham.islam@ithaka.org>
2021-06-16 14:55:49 -0400

    docs: undo previos renaming of contributing-documentation and remove accessibility header

--- packages/pharos-site/src/components/Sidenav.tsx
+++ packages/pharos-site/src/components/Sidenav.tsx
@@ -77,7 +77,7 @@ const Sidenav: FC = () => {
             Help
           </PharosSidenavLink>
           <PharosSidenavMenu label="Contributing" expanded={isExpanded('contributing')}>
-            {['Writing documentation'].map(createSidenavLink.bind(this, 'contributing'))}
+            {['Contributing documentation'].map(createSidenavLink.bind(this, 'contributing'))}
           </PharosSidenavMenu>
           <PharosSidenavLink href="https://github.com/ithaka/pharos" target="_blank" external>
             GitHub
