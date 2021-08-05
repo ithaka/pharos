0a90f179f48a8cde74c42ba32955e9887a993fa1
Robert Niznik <robert.niznik@ithaka.org>
2021-08-05 14:01:05 -0400

    feat: use esbuild-loader for Storybook (#167)
    on-behalf-of: @ithaka <robert.niznik@ithaka.org>

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
+const { ProvidePlugin } = require('webpack');
 
 // Export a function. Accept the base config as the only param.
 module.exports = async ({ config, mode }) => {
@@ -41,6 +43,32 @@ module.exports = async ({ config, mode }) => {
   });
 
   config.module.rules[1].exclude = /node_modules/;
+
+  // TODO: Refactor when Chromatic updates Firefox
+  const TARGET_ENV = process.env.GITHUB_WORKFLOW === 'Chromatic' ? 'firefox65' : 'esnext';
+  config.module.rules.forEach((rule) => {
+    if (
+      /babel-loader/i.test(rule.loader) ||
+      /babel-loader/i.test(rule.use?.loader) ||
+      (rule.use?.length === 1 && rule.use?.some((use) => /babel-loader/i.test(use.loader)))
+    ) {
+      delete rule.use;
+      rule.loader = 'esbuild-loader';
+      rule.options = { target: TARGET_ENV };
+    }
+  });
+
+  config.module.rules[0].options = { loader: 'tsx', target: TARGET_ENV };
+
+  if (config.mode === 'production') {
+    config.optimization.minimizer = [new ESBuildMinifyPlugin({ target: TARGET_ENV })];
+  }
+
+  config.plugins.push(
+    new ProvidePlugin({
+      React: 'react',
+    })
+  );
 
   // Return the altered config
   return config;
