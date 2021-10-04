414b4c0071d613fdd1fea51b5d53b6078f3cf50b
Niznikr <robert.niznik@ithaka.org>
2021-10-04 11:07:15 -0400

    fix: address site build error
    on-behalf-of: @ithaka <robert.niznik@ithaka.org>

--- packages/pharos-site/src/typings/declarations.d.ts
+++ packages/pharos-site/src/typings/declarations.d.ts
@@ -2,6 +2,3 @@ declare module '*.module.css';
 declare module '*.svg';
 declare module '*.woff2';
 declare module '*.png';
-declare module 'prettier/esm/standalone.mjs';
-declare module 'prettier/esm/parser-html.mjs';
-declare module 'prettier/esm/parser-babel.mjs';
