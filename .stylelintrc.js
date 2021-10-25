8ce807491a943004e8fe50d78c3fd1fd8e25a9a8
Niznikr <robert.niznik@ithaka.org>
2021-10-25 10:21:36 -0400

    fix: address errors
    on-behalf-of: @ithaka <robert.niznik@ithaka.org>

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-config-prettier', 'stylelint-config-standard-scss'],
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier'],
   plugins: ['stylelint-scss'],
   rules: {
     'length-zero-no-unit': [true, { ignore: 'custom-properties' }],
@@ -9,5 +9,8 @@ module.exports = {
     'declaration-no-important': true,
     'string-quotes': 'single',
     'selector-class-pattern': null,
+    'selector-id-pattern': null,
+    'keyframes-name-pattern': null,
+    'scss/operator-no-newline-after': null,
   },
 };
