2bc785d16f30c954c53fdcfcd39a65600c866476
Robert Niznik <robert.niznik@ithaka.org>
2021-10-25 13:06:10 -0400

    chore: update Stylelint (#243)
    on-behalf-of: @ithaka <robert.niznik@ithaka.org>

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier'],
   plugins: ['stylelint-scss'],
   rules: {
     'length-zero-no-unit': [true, { ignore: 'custom-properties' }],
@@ -7,5 +7,12 @@ module.exports = {
     'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': true,
     'declaration-no-important': true,
+    'string-quotes': 'single',
+    'selector-class-pattern': null,
+    'selector-id-pattern': null,
+    'keyframes-name-pattern': null,
+    'scss/operator-no-newline-after': null,
+    'value-no-vendor-prefix': [true, { ignoreValues: 'box' }],
+    'alpha-value-notation': 'number',
   },
 };
