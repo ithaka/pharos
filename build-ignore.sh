c6c689ccaedd481d96ca274d8e9510703619aeca
Dane Hillard <github@danehillard.com>
2022-07-12 14:25:20 -0400

    fix(build): always build if the compared commits are the same

--- build-ignore.sh
+++ build-ignore.sh
@@ -2,6 +2,11 @@
 
 echo "Checking for files changed between $CACHED_COMMIT_REF and $COMMIT_REF..."
 
+# Build if the compared commits are identical
+if [ "$CACHED_COMMIT_REF" == "$COMMIT_REF" ]; then
+  exit 0
+fi
+
 if [ "$SITE_NAME" == "pharos-storybooks" ]
 then
   git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF packages/pharos/ .storybook/ package.json yarn.lock netlify.toml
