65c661342905b1c2e9d2901134717a77f6c4efc8
Evan Shoup <evan.shoup@ithaka.org>
2022-10-05 08:15:26 -0400

    refactor(sidenav): distinct naming conventions

--- packages/pharos/src/components/sidenav/pharos-sidenav.ts
+++ packages/pharos/src/components/sidenav/pharos-sidenav.ts
@@ -18,7 +18,7 @@ import { PharosLink } from '../link/pharos-link';
  * @slot top - Content to be shown at the top of the sidenav.
  * @slot - Contains the sections of the sidenav (the default slot).
  *
- * @fires close - Fires when the sidenav has closed
+ * @fires pharos-sidenav-close - Fires when the sidensav has closed
  *
  */
 export class PharosSidenav extends ScopedRegistryMixin(FocusMixin(SideElement)) {
@@ -67,15 +67,17 @@ export class PharosSidenav extends ScopedRegistryMixin(FocusMixin(SideElement)) 
     event.stopPropagation();
 
     this.slide = false;
-    const details = {
-      bubbles: true,
-      composed: true,
-    };
-    this.dispatchEvent(new Event('close', details));
     const button: PharosSidenavButton | null = document.querySelector(
       '[data-pharos-component="PharosSidenavButton"]'
     );
     button?.focus();
+
+    const details = {
+      bubbles: true,
+      composed: true,
+      detail: this,
+    };
+    this.dispatchEvent(new CustomEvent('pharos-sidenav-close', details));
   }
 
   private _handleMediaChange(e: MediaQueryListEvent): void {
