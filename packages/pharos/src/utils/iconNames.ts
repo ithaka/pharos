1c7104f677975a0e53e0be369cff14de9f71a1d9
Dane Hillard <github@danehillard.com>
2022-11-05 15:04:03 -0400

    chore(storybook): convert Icon stories to CSF 3

--- packages/pharos/src/utils/iconNames.ts
+++ packages/pharos/src/utils/iconNames.ts
@@ -1,3 +1,4 @@
 import tokens from '../styles/tokens';
+import type { IconName } from '../components/icon/pharos-icon';
 
-export const iconNames = Object.keys(tokens.asset.icon).sort();
+export const iconNames = Object.keys(tokens.asset.icon).sort() as IconName[];
