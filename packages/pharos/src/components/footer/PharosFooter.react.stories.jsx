8b04c0b3ddc1c90bce09a69f5e0019f71daea386
Dane Hillard <github@danehillard.com>
2022-11-05 14:10:06 -0400

    chore(storybook): convert Footer stories to CSF 3

---
+++ packages/pharos/src/components/footer/PharosFooter.react.stories.jsx
@@ -0,0 +1,19 @@
+import { Footer } from '../../pages/shared/react/footer';
+import { PharosFooter } from '../../../lib/react-components';
+import { configureDocsPage } from '@config/docsPageConfig';
+
+export default {
+  title: 'Organisms/Footer',
+  component: PharosFooter,
+  parameters: {
+    docs: { page: configureDocsPage('footer') },
+    layout: 'fullscreen',
+  },
+};
+
+export const Base = {
+  render: (_) => <Footer />,
+  parameters: {
+    chromatic: { viewports: [320, 1024, 1200] },
+  },
+};
