9f8d1dec607dc9c6dabd7a83fe493707088e1ada
Dane Hillard <github@danehillard.com>
2022-11-05 11:32:20 -0400

    chore(storybook): convert Combobox stories to CSF 3

---
+++ packages/pharos/src/components/combobox/storyArgs.ts
@@ -0,0 +1,13 @@
+export const defaultArgs = {
+  disabled: false,
+  hideLabel: false,
+  invalidated: false,
+  label: 'Combobox Label',
+  message: '',
+  name: '',
+  open: false,
+  placeholder: '',
+  required: false,
+  validated: false,
+  value: '',
+};
