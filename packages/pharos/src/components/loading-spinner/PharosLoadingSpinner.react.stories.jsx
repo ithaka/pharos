23fc9ad54f59e5de62661329f4f7a88820881054
Dane Hillard <github@danehillard.com>
2022-11-05 20:15:45 -0400

    chore(storybook): convert LoadingSpinner stories to CSF 3

---
+++ packages/pharos/src/components/loading-spinner/PharosLoadingSpinner.react.stories.jsx
@@ -0,0 +1,40 @@
+import { action } from '@storybook/addon-actions';
+
+import { PharosLoadingSpinner, PharosHeading, PharosButton } from '../../react-components';
+import { configureDocsPage } from '../../../../../.storybook/docsPageConfig.jsx';
+
+export default {
+  title: 'Components/Loading Spinner',
+  component: PharosLoadingSpinner,
+  parameters: {
+    docs: { page: configureDocsPage('loading-spinner') },
+    options: { selectedPanel: 'addon-controls' },
+  },
+};
+
+export const Base = {
+  render: (_) => (
+    <div>
+      <PharosLoadingSpinner></PharosLoadingSpinner>
+      <PharosHeading level={1} preset={'5'}>
+        Loading spinner demonstration
+      </PharosHeading>
+      <PharosButton onClick={() => action('Click')('Clicked')}>Can't press me!</PharosButton>
+    </div>
+  ),
+};
+
+export const Scoped = {
+  render: (_) => (
+    <div
+      style={{
+        height: '5rem',
+        width: '5rem',
+        border: '1px solid black',
+        position: 'relative',
+      }}
+    >
+      <PharosLoadingSpinner></PharosLoadingSpinner>
+    </div>
+  ),
+};
