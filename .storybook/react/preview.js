b6fce226a81e83f3b247e4551ae66d76210b8305
Dane Hillard <github@danehillard.com>
2022-12-11 11:20:25 -0500

    feat(storybook): use Pharos standard colors for background options (#464)

--- .storybook/react/preview.js
+++ .storybook/react/preview.js
@@ -1,27 +1,4 @@
-import { Canvas } from '@storybook/addon-docs';
-import { INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
-
-import a11yConfig from '../a11yConfig';
 import '../styleConfig';
-import theme from '../theme';
 import '../../packages/pharos/src/test/initComponents';
 
-export const parameters = {
-  a11y: a11yConfig,
-  controls: { expanded: true },
-  docs: {
-    source: { type: 'dynamic' },
-    inlineStories: true,
-    theme: theme,
-    components: {
-      Canvas: Canvas,
-    },
-  },
-  viewport: {
-    viewports: {
-      ...MINIMAL_VIEWPORTS,
-      ...INITIAL_VIEWPORTS,
-    },
-    defaultViewport: 'responsive',
-  },
-};
+export { parameters } from '../main/preview';
