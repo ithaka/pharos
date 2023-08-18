2244c29543ab6ca4b230cc1b7f4d74ed663e2e7d
Mike Iden <mike.iden@ithaka.org>
2023-08-18 15:35:46 -0400

    feat(popover): adding react examples

--- packages/pharos/src/components/popover/PharosPopover.react.stories.jsx
+++ packages/pharos/src/components/popover/PharosPopover.react.stories.jsx
@@ -1,4 +1,5 @@
 import { PharosPopover } from '../../react-components/popover/pharos-popover';
+import { PharosButton } from '../../react-components/button/pharos-button';
 import { configureDocsPage } from '@config/docsPageConfig';
 import { PharosContext } from '../../utils/PharosContext';
 
@@ -20,6 +21,98 @@ export default {
 };
 
 export const Base = {
-  render: (args) => <PharosPopover />,
-  args: {},
+  render: () => (
+    <div>
+      <PharosButton id="my-button" data-popover-id="my-popover" icon-right="chevron-down">
+        Click Me
+      </PharosButton>
+      <PharosPopover id="my-popover">
+        <div style="padding: 1rem">Some very simple contents</div>
+      </PharosPopover>
+    </div>
+  ),
+};
+
+export const Events = {
+  render: () => (
+    <div>
+      <PharosButton id="my-button" data-popover-id="my-popover" icon-right="chevron-down">
+        Click Me
+      </PharosButton>
+      <PharosPopover id="my-popover">
+        <div style="padding: 1rem;display: flex; flex-direction: column; gap: 1rem;">
+          <div style="padding: 1rem">Some very simple contents</div>
+          <PharosButton
+            onClick={() => {
+              const menu = document.querySelector('storybook-pharos-popover');
+              menu.open = false;
+            }}
+          >
+            Close
+          </PharosButton>
+        </div>
+      </PharosPopover>
+    </div>
+  ),
+};
+
+export const Background = {
+  render: () => (
+    <div>
+      <PharosButton id="my-button" data-popover-id="my-popover" icon-right="chevron-down">
+        Click Me
+      </PharosButton>
+      <PharosPopover id="my-popover">
+        <div style="background: #444444; color: white; padding: 1rem;display: flex; flex-direction: column; gap: 1rem;">
+          <div style="padding: 1rem">Some very simple contents</div>
+          <PharosButton
+            onClick={() => {
+              const menu = document.querySelector('storybook-pharos-popover');
+              menu.open = false;
+            }}
+          >
+            Close
+          </PharosButton>
+        </div>
+      </PharosPopover>
+    </div>
+  ),
+};
+
+export const LargeContents = {
+  render: () => (
+    <div>
+      <PharosButton id="my-button" data-popover-id="my-popover" icon-right="chevron-down">
+        Click Me
+      </PharosButton>
+      <PharosPopover id="my-popover">
+        <div style="padding: 1rem;display: flex; flex-direction: column; gap: 1rem;">
+          <div style="height: 200px; overflow: auto; border: 1px solid black; padding: 1rem">
+            <div>Some really cool stuff</div>
+            <div>Some really cool stuff</div>
+            <div>Some really cool stuff</div>
+            <div>Some really cool stuff</div>
+            <div>Some really cool stuff</div>
+            <div>Some really cool stuff</div>
+            <div>Some really cool stuff</div>
+            <div>Some really cool stuff</div>
+            <div>Some really cool stuff</div>
+            <div>Some really cool stuff</div>
+            <div>Some really cool stuff</div>
+            <div>Some really cool stuff</div>
+            <div>Some really cool stuff</div>
+            <div>Some really cool stuff</div>
+          </div>
+          <PharosButton
+            onClick={() => {
+              const menu = document.querySelector('storybook-pharos-popover');
+              menu.open = false;
+            }}
+          >
+            Close
+          </PharosButton>
+        </div>
+      </PharosPopover>
+    </div>
+  ),
 };
