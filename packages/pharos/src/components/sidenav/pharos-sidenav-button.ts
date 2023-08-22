e82f079b6d1b937eae4ecfaefc76f8de72d01b57
Mat Harris <mat.harris@ithaka.org>
2023-08-22 17:08:08 -0400

    feat(button): add reference to new button types
    Including the ExpandedState type reference for
    other components that consume the button. These
    threw errors when initially compiling so there
    may be other such commponents that eventually
    need a similar update.

---
+++ packages/pharos/src/components/sidenav/pharos-sidenav-button.ts
@@ -0,0 +1,48 @@
+import type { CSSResultArray } from 'lit';
+import { sidenavButtonStyles } from './pharos-sidenav-button.css';
+import { PharosButton } from '../button/pharos-button';
+import type { PharosSidenav } from './pharos-sidenav';
+
+import type { LinkTarget } from '../base/anchor-element';
+import type {
+  ButtonType,
+  IconName,
+  ButtonVariant,
+  PressedState,
+  ExpandedState,
+} from '../button/pharos-button';
+export type { LinkTarget, ButtonType, IconName, ButtonVariant, PressedState, ExpandedState };
+
+/**
+ * Pharos sidenav button component.
+ *
+ * @tag pharos-sidenav-button
+ *
+ */
+export class PharosSidenavButton extends PharosButton {
+  constructor() {
+    super();
+    this.icon = 'menu';
+    this.variant = 'subtle';
+    this.a11yLabel = 'Open menu';
+  }
+
+  public static override get styles(): CSSResultArray {
+    return [super.styles, sidenavButtonStyles];
+  }
+
+  protected override firstUpdated(): void {
+    this.addEventListener('click', this._handleClickOpen);
+  }
+
+  private async _handleClickOpen(): Promise<void> {
+    const sidenav: PharosSidenav | null = document.querySelector(
+      '[data-pharos-component="PharosSidenav"]'
+    );
+    if (sidenav) {
+      sidenav.slide = true;
+      await sidenav.updateComplete;
+      sidenav.focus();
+    }
+  }
+}
