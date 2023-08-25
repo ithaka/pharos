ec16119dca80fc4a796840d90165a217bf4fa043
Henry Long <henry.long@ithaka.org>
2023-08-25 11:15:34 -0400

    feat: build out basic coach mark structure using pharos components

--- packages/pharos/src/components/coach-mark/pharos-coach-mark.ts
+++ packages/pharos/src/components/coach-mark/pharos-coach-mark.ts
@@ -1,4 +1,7 @@
 import { PharosElement } from '../base/pharos-element';
+import { PharosButton } from '../button/pharos-button';
+import { PharosHeading } from '../heading/pharos-heading';
+import ScopedRegistryMixin from '../../utils/mixins/scoped-registry';
 import { html } from 'lit';
 import type { TemplateResult, CSSResultArray } from 'lit';
 import { coachMarkStyles } from './pharos-coach-mark.css';
@@ -9,12 +12,30 @@ import { coachMarkStyles } from './pharos-coach-mark.css';
  * @tag pharos-coach-mark
  *
  */
-export class PharosCoachMark extends PharosElement {
+export class PharosCoachMark extends ScopedRegistryMixin(PharosElement) {
+  static elementDefinitions = {
+    'pharos-button': PharosButton,
+    'pharos-heading': PharosHeading,
+  };
+
   public static override get styles(): CSSResultArray {
     return [coachMarkStyles];
   }
 
   protected override render(): TemplateResult {
-    return html` <span>This is the CoachMark component!</span> `;
+    return html`
+      <div class="coach-mark">
+        <pharos-button
+          id="close-button"
+          class="coach-mark__close"
+          type="button"
+          variant="subtle"
+          icon="close"
+          label="Close coach mark"
+        ></pharos-button>
+        <pharos-heading level="2" preset="1">Lorem Ipsum</pharos-heading>
+        <p>This is the CoachMark component</p>
+      </div>
+    `;
   }
 }
