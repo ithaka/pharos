44037be1c049bb84bad28ce34fe0b8babb133780
Henry Long <henry.long@ithaka.org>
2023-09-07 14:53:29 -0400

    feat: add coach mark unit tests

--- packages/pharos/src/components/coach-mark/pharos-coach-mark.test.ts
+++ packages/pharos/src/components/coach-mark/pharos-coach-mark.test.ts
@@ -3,12 +3,15 @@ import { html } from 'lit/static-html.js';
 import sinon from 'sinon';
 import type { SinonSpy } from 'sinon';
 import type { PharosCoachMark } from './pharos-coach-mark';
+import type { PharosButton } from '../button/pharos-button';
 
 describe('pharos-coach-mark', () => {
   let component: PharosCoachMark, logSpy: SinonSpy;
 
   beforeEach(async () => {
-    component = await fixture(html` <pharos-coach-mark> Shell CoachMark </pharos-coach-mark> `);
+    component = await fixture(
+      html`<test-pharos-coach-mark header="Test Header">Test Description</test-pharos-coach-mark>`
+    );
   });
 
   before(() => {
@@ -20,6 +23,53 @@ describe('pharos-coach-mark', () => {
   });
 
   it('is accessible', async () => {
-    await expect(component).to.be.accessible();
+    expect(component).to.be.accessible();
+  });
+
+  it('is accessible when opened', async () => {
+    component.hide = false;
+    await component.updateComplete;
+    expect(component).to.be.accessible();
+  });
+
+  it('has an attribute to open the coach mark', async () => {
+    component.hide = false;
+    await component.updateComplete;
+    expect(component.hide).to.be.false;
+  });
+
+  it('has an attribute to close the coach mark', async () => {
+    component.hide = false;
+    await component.updateComplete;
+
+    component.hide = true;
+    await component.updateComplete;
+    expect(component.hide).to.be.true;
+  });
+
+  it('closes when the close button is clicked', async () => {
+    component.hide = false;
+    await component.updateComplete;
+
+    const closeButton = component.renderRoot.querySelector('#close-button') as PharosButton;
+    closeButton.click();
+    await component.updateComplete;
+
+    expect(component.hide).to.be.true;
+  });
+
+  it('displays the header set in the element attribute', async () => {
+    component = await fixture(
+      html`<test-pharos-coach-mark header="Test Header">Test Description</test-pharos-coach-mark>`
+    );
+    const header = component.renderRoot.querySelector('pharos-heading');
+    expect(header).to.have.text('Test Header');
+  });
+
+  it('displays content added as a child to the element', async () => {
+    component = await fixture(
+      html`<test-pharos-coach-mark>Test Description</test-pharos-coach-mark>`
+    );
+    expect(component).to.have.text('Test Description');
   });
 });
