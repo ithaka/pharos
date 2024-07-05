b10fcffada9de104cd446ca61a6cf672012c0803
Brent Swisher <brent.swisher@ithaka.org>
2024-07-05 09:25:27 -0400

    fix(site): remove extra imports from ssr config

--- packages/pharos-site/gatsby-ssr.js
+++ packages/pharos-site/gatsby-ssr.js
@@ -1,4 +1,1 @@
-import '@webcomponents/scoped-custom-element-registry';
-import '@ithaka/pharos/lib/styles/typography.scss';
-
 export { wrapRootElement, wrapPageElement } from './wrap-root-element';
