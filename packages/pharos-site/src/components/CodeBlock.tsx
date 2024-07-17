1281bd074d7906bec3c911d51454dd230ad04b44
Brent Swisher <brent.swisher@ithaka.org>
2024-07-17 08:58:10 -0400

    feat(site): simplify CodeBlock component
    Remove the live and render options and updates it to take
    a string in instead of a children prop.

--- packages/pharos-site/src/components/CodeBlock.tsx
+++ packages/pharos-site/src/components/CodeBlock.tsx
@@ -1,66 +1,23 @@
-import type { FC, ReactElement } from 'react';
-import { useEffect, useState } from 'react';
-import ReactDOMServer from 'react-dom/server';
-import { Highlight } from 'prism-react-renderer';
+import type { FC } from 'react';
+import { Highlight, themes } from 'prism-react-renderer';
 import type { Language } from 'prism-react-renderer';
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
-import prettier from 'prettier';
-import parserHtml from 'prettier/parser-html';
 
 interface CodeBlockProps {
-  className?: string;
-  live?: boolean;
-  render?: boolean;
+  code: string;
+  language?: Language;
 }
 
-const CodeBlock: FC<CodeBlockProps> = ({ children, className, live, render }) => {
-  const [code, setCode] = useState<string>('');
-
-  useEffect(() => {
-    async function formatCode() {
-      const snippet = ReactDOMServer.renderToStaticMarkup(children as ReactElement);
-      if (snippet) {
-        /*
-        const formattedCode = await prettier.format(snippet, {
-          parser: 'html',
-          plugins: [parserHtml],
-        });
-        setCode(formattedCode);
-        */
-        setCode(snippet);
-      }
-    }
-    formatCode();
-  }, []);
-
-  if (live) {
-    return (
-      <div style={{ marginTop: '40px', backgroundColor: 'black' }}>
-        <LiveProvider code={code.trim()} transformCode={(code) => '/** @jsx */' + code}>
-          <LivePreview />
-          <LiveEditor />
-          <LiveError />
-        </LiveProvider>
-      </div>
-    );
-  }
-
-  if (render) {
-    return (
-      <div style={{ marginTop: '40px' }}>
-        <LiveProvider code={code}>
-          <LivePreview />
-        </LiveProvider>
-      </div>
-    );
-  }
-
-  const language = className?.replace(/language-/, '') as Language;
-
+const CodeBlock: FC<CodeBlockProps> = ({ code, language = 'tsx' }) => {
+  const codeStyles: React.CSSProperties = {
+    padding: '20px',
+    whiteSpace: 'normal',
+    fontSize: 'var(--pharos-font-size-medium)',
+    fontFamily: 'var(--pharos-font-family-sans-serif)',
+  };
   return (
-    <Highlight code={code.trim()} language={language}>
+    <Highlight code={code.trim()} language={language} theme={themes.nightOwl}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: '20px', whiteSpace: 'normal' }}>
+        <pre className={className} style={{ ...style, ...codeStyles }}>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
