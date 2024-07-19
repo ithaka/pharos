90dbc66ad6d474ac8a34baadffa2a53e29e725fd
Brent Swisher <brent@brentswisher.com>
2024-07-19 16:07:48 -0400

    Fix pharos-site sidenav on mobile screens (#782)
    * feat(site): toggle sidenav on mobile sizes
    
    When Pharos v14 was release the default behavior of the sidenav changed
    and broke the site navigation
    
    * fix(site): add a check before using the window object for Gatsby SSR

--- packages/pharos-site/src/components/layout.tsx
+++ packages/pharos-site/src/components/layout.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import type { FC, ReactElement } from 'react';
 import type { WindowLocation } from '@reach/router';
 import Sidenav from './Sidenav';
@@ -36,35 +36,70 @@ const Layout: FC<LayoutProps> = ({ children, location, fill }) => {
     typeof window !== `undefined` ? require('@ithaka/pharos/lib/react-components') : null;
 
   const [MainContent, setMainContent] = useState<ReactElement | null>(null);
+  const mobileBreakpoint = 1055;
+  const [isMobile, setIsMobile] = useState<boolean>(
+    typeof window !== `undefined` ? window.innerWidth < mobileBreakpoint : false
+  );
+  const [isSidenavDisplayed, setIsSidenavDisplayed] = useState<boolean>(!isMobile);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const { PharosLink, PharosLayout } = Pharos;
+    const { PharosLink, PharosLayout, PharosButton } = Pharos;
+    const currentRef = containerRef.current;
+    const resizeObserver = new ResizeObserver(() => {
+      const windowWidth = window.innerWidth;
+      setIsSidenavDisplayed(windowWidth >= mobileBreakpoint);
+      setIsMobile(windowWidth < mobileBreakpoint);
+    });
 
     const body = (
-      <main className={main}>
-        <div className={topBar}>
-          <PharosLink id="skip-link" skip href="#sidenav-skip-link" isOnBackground>
-            Skip to main navigation
-          </PharosLink>
-        </div>
-        <PharosLayout
-          preset="1-col--sidenav-comfy"
-          className={`${content} ${fill ? content___fill : ''}`}
-          rowGap={fill ? PharosSpacing5X : '0'}
-        >
-          {fill ? children : <div className={page}>{children}</div>}
-        </PharosLayout>
-      </main>
+      <>
+        <Sidenav isOpen={isSidenavDisplayed} showCloseButton={isMobile} />
+        <main className={main}>
+          <div className={topBar}>
+            <PharosLink id="skip-link" skip href="#sidenav-skip-link" isOnBackground>
+              Skip to main navigation
+            </PharosLink>
+          </div>
+          <PharosLayout preset="1-col--sidenav-comfy" rowGap={fill ? PharosSpacing5X : '0'}>
+            <PharosButton
+              variant="subtle"
+              icon="menu"
+              data-sidenav-id="site-sidenav"
+              a11yLabel="Open side navigation"
+              style={{
+                display: isMobile ? 'block' : 'none',
+                paddingTop: 'var(--pharos-spacing-2-x)',
+              }}
+            />
+          </PharosLayout>
+          <PharosLayout
+            preset="1-col--sidenav-comfy"
+            className={`${content} ${fill ? content___fill : ''}`}
+            rowGap={fill ? PharosSpacing5X : '0'}
+          >
+            {fill ? children : <div className={page}>{children}</div>}
+          </PharosLayout>
+        </main>
+      </>
     );
 
     setMainContent(body);
-  }, [Pharos, children, fill]);
+
+    if (currentRef) {
+      resizeObserver.observe(currentRef);
+    }
+    return () => {
+      if (currentRef) {
+        resizeObserver.unobserve(currentRef);
+      }
+    };
+  }, [Pharos, children, fill, isMobile, isSidenavDisplayed]);
 
   return (
-    <div className={container}>
+    <div className={container} ref={containerRef}>
       <Fonts />
       <SEO title={pageName || 'Home'} pathname={location?.pathname} />
-      <Sidenav />
       {MainContent}
       <Footer />
     </div>
