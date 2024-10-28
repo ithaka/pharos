fd8d91e5516888ec60a69b8d2488ee6c291c733c
Mike Iden <mike.iden@ithaka.org>
2024-10-28 11:48:29 -0400

    fix(modal): fix node 22 import assertion syntax

--- packages/pharos/style-dictionary.config.js
+++ packages/pharos/style-dictionary.config.js
@@ -1,4 +1,4 @@
-import icons from './tokens/assets/icons.json' assert { type: 'json' };
+import icons from './tokens/assets/icons.json' with { type: 'json' };
 
 export default {
   source: ['tokens/**/*.json'],
