d9039dcb8059d2fa51cf82ba869d682e22ddb879
Mat Harris <mat.harris@ithaka.org>
2025-04-14 16:04:04 -0400

    docs(breadcrumbs): update accessibility section

--- packages/pharos-site/static/guidelines/breadcrumb.docs.tsx
+++ packages/pharos-site/static/guidelines/breadcrumb.docs.tsx
@@ -20,7 +20,7 @@ const BreadcrumbPage: FC = () => {
         <>
           <PharosBreadcrumb>
             <PharosBreadcrumbItem href="#">
-              Mouse Over to See the Full Text of Long Content Which Are Truncated
+              Mouse Over or Focus to See the Full Text of Long Content Which Are Truncated
             </PharosBreadcrumbItem>
             <PharosBreadcrumbItem href="#">Short Texts Will Not</PharosBreadcrumbItem>
             <PharosBreadcrumbItem>The Current Place</PharosBreadcrumbItem>
@@ -53,24 +53,43 @@ const BreadcrumbPage: FC = () => {
         />
       </PageSection>
       <PageSection title="Accessibility">
-        <PageSection subSectionLevel={1} title="Relevant WCAG guidelines">
+        <PageSection title="What's built in" subSectionLevel={1}>
           <ul>
             <li>
-              <PharosLink href="https://www.w3.org/TR/wai-aria-practices/#breadcrumb">
-                3.4 Authoring practices
-              </PharosLink>
+              Ensures component uses the correct semantic <code>nav</code> element with an{' '}
+              <code>aria-label="breadcrumb"</code>.
             </li>
             <li>
-              <PharosLink href="https://www.w3.org/WAI/WCAG21/Understanding/location.html">
-                2.4.8 Location (AA)
-              </PharosLink>
+              Utilized <code>ol</code> to programmatically group each breadcrumb item in the list.
             </li>
             <li>
-              <PharosLink href="https://www.w3.org/WAI/WCAG21/Understanding/name-role-value.html">
-                4.1.2: Name, Role, Value (Level A)
-              </PharosLink>
+              Each listitem has <code>role="listitem"</code> to ensure that the list association is
+              properly conveyed.
             </li>
+            <li>
+              Provides built-in focus styles that meet WCAG contrast and visibility requirements.
+            </li>
+            <li>Truncated text can be exposed via hover or keyboard focus.</li>
           </ul>
+        </PageSection>
+        <PageSection title="Considerations" subSectionLevel={1}>
+          <PageSection title="Design" subSectionLevel={2}>
+            <ul>
+              <li>
+                Breadcrumb placement is consistent across the site and is placed above the main
+                content area.
+              </li>
+              <li>Ensure the breadcrumb is not used as a primary navigation element.</li>
+            </ul>
+          </PageSection>
+          <PageSection title="Development" subSectionLevel={2}>
+            <ul>
+              <li>
+                Verify the <code>pharos-breadcrumb-item</code> has a valid <code>href</code>{' '}
+                attribute.
+              </li>
+            </ul>
+          </PageSection>
         </PageSection>
         <PageSection
           subSectionLevel={1}
@@ -82,14 +101,6 @@ const BreadcrumbPage: FC = () => {
           title="Visual expectations"
           description="A trail of links pertaining to parental content separated by forward slashes"
         />
-        <PageSection subSectionLevel={1} title="Code expectations">
-          <ul>
-            <li>
-              The parent breadcrumb component is a nav element with an aria-label of breadcrumb
-            </li>
-            <li>The collection of links are contained in a ordered list</li>
-          </ul>
-        </PageSection>
         <PageSection title="Expected actions" subSectionLevel={1}>
           <PageSection
             title="Screen reader"
@@ -101,6 +112,20 @@ const BreadcrumbPage: FC = () => {
               <li>Links in breadcrumb are focused in order with tab</li>
             </ul>
           </PageSection>
+        </PageSection>
+        <PageSection subSectionLevel={1} title="Relevant best practices and WCAG guidelines">
+          <ul>
+            <li>
+              <PharosLink href="https://www.w3.org/WAI/ARIA/apg/patterns/breadcrumb/">
+                Breadcrumb pattern from WAI-ARIA Authoring Practices
+              </PharosLink>
+            </li>
+            <li>
+              <PharosLink href="https://www.w3.org/WAI/WCAG21/Understanding/name-role-value.html">
+                4.1.2: Name, Role, Value (Level A)
+              </PharosLink>
+            </li>
+          </ul>
         </PageSection>
       </PageSection>
     </>
