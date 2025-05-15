9dc4a722e2fa3354d4af4c34ca32628f269f0d28
Brent Swisher <brent.swisher@ithaka.org>
2025-05-15 10:50:41 -0400

    feat: copy combobox for start of internals of multiselect-dropdown

---
+++ packages/pharos/src/components/multiselect-dropdown/storyArgs.ts
@@ -0,0 +1,14 @@
+export const defaultArgs = {
+  disabled: false,
+  hideLabel: false,
+  invalidated: false,
+  label: 'Combobox Label',
+  looseMatch: false,
+  message: '',
+  name: '',
+  open: false,
+  placeholder: '',
+  required: false,
+  validated: false,
+  value: '',
+};
