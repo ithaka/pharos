a313ee37152f9310fb5bd584db252f16b008563d
Mat Harris <mat.harris@ithaka.org>
2025-06-26 13:31:34 -0700

    fix(search results): remove redundant image card image alt

--- packages/pharos/src/pages/search/wc/image-results/image-results-item.ts
+++ packages/pharos/src/pages/search/wc/image-results/image-results-item.ts
@@ -5,7 +5,7 @@ import type { ImageResult } from '../../types';
 export const ImageResultsItem = (result: ImageResult): TemplateResult => html`
   <storybook-pharos-link href="#" subtle flex>
     <div class="search-page__grid--image">
-      <img src="${result.image}" alt="${result.title}" width="99%" />
+      <img src="${result.image}" alt="" width="99%" />
       <storybook-pharos-heading preset="1--bold" level="3" no-margin
         >${result.title}</storybook-pharos-heading
       >
