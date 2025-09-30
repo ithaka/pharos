c45e0c7169ac320599108c63d4ad30d17de1cf10
Brent Swisher <brent.swisher@ithaka.org>
2025-09-30 14:31:54 -0400

    docs(pill): setup controls and rearrange stories

--- packages/pharos/src/components/pill/storyArgs.ts
+++ packages/pharos/src/components/pill/storyArgs.ts
@@ -1,9 +1,16 @@
 export interface ComponentArgs {
   size: 'small' | 'base';
+  dismissible: boolean;
+  disabled: boolean;
+  preset: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
+  'icon-left'?: string;
 }
 
 export type StoryArgs = ComponentArgs & {};
 
 export const defaultArgs: StoryArgs = {
   size: 'base',
+  dismissible: false,
+  disabled: false,
+  preset: '1',
 };
