c47a76f33e211aee5faac1063c18abdd6e206b35
Brent Swisher <brent.swisher@ithaka.org>
2026-06-11 08:31:28 -0400

    test: update to use vitest instead of wtr as the test runner

---
+++ packages/pharos/vitest.setup.ts
@@ -0,0 +1,5 @@
+// Polyfill for custom-element definitions.
+import '@webcomponents/scoped-custom-element-registry';
+
+// Register every Pharos component under the `test-` prefix
+import './src/test/initComponents';
