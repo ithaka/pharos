264dd533c06b553267e30b4e8785834a209f48a7
Brent Swisher <brent.swisher@ithaka.org>
2026-07-01 14:42:14 -0400

    test(alert): migrate tests from @open-wc/testing to vitest

--- packages/pharos/vitest.setup.ts
+++ packages/pharos/vitest.setup.ts
@@ -3,3 +3,6 @@ import '@webcomponents/scoped-custom-element-registry';
 
 // Register every Pharos component under the `test-` prefix
 import './src/test/initComponents';
+
+// Register custom matchers globally
+import './src/test/matchers';
