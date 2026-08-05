d088591d37980d914f241d6f89bfedb2a52451e9
Dane Hillard <github@danehillard.com>
2026-08-04 21:34:35 -0400

    fix(tooltip): update timeout in tests

--- packages/pharos/src/components/tooltip/pharos-tooltip.test.ts
+++ packages/pharos/src/components/tooltip/pharos-tooltip.test.ts
@@ -141,10 +141,13 @@ describe('pharos-tooltip', () => {
     expect(secondComponent.open).toBe(false);
 
     secondTrigger.dispatchEvent(new Event('mouseenter'));
-    await vi.waitFor(() => {
-      expect(component.open).toBe(false);
-      expect(secondComponent.open).toBe(true);
-    });
+    await vi.waitFor(
+      () => {
+        expect(component.open).toBe(false);
+        expect(secondComponent.open).toBe(true);
+      },
+      { timeout: 2000 }
+    );
   });
 
   it('opens the first one on hover and then closes it upon focusing the second', async () => {
@@ -153,10 +156,13 @@ describe('pharos-tooltip', () => {
     expect(secondComponent.open).toBe(false);
 
     secondTrigger.dispatchEvent(new Event('focusin'));
-    await vi.waitFor(() => {
-      expect(component.open).toBe(false);
-      expect(secondComponent.open).toBe(true);
-    });
+    await vi.waitFor(
+      () => {
+        expect(component.open).toBe(false);
+        expect(secondComponent.open).toBe(true);
+      },
+      { timeout: 2000 }
+    );
   });
 
   it('throws an error for invalid fallback values', async () => {
